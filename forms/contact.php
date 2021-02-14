d62bd2df51ad8af48560aa23471c771828d80481
prakash <pmishra@360alumni.com>
2021-02-14 21:03:11 +0530

    name changed

--- forms/contact.php
+++ forms/contact.php
@@ -7,35 +7,25 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'pcmishra22@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if(isset($_POST['email'])){
+    $to = $receiving_email_address; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $subject = $_POST['subject'];
+   
+    $message = $name .  " wrote the following:" . "\n\n" . $_POST['message'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    if (mail($to,$subject,$message,$headers))
+      echo "Mail Sent. Thank you !" . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    else
+       echo "Mail Not Sent, Please try again !";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    }
 
-  echo $contact->send();
 ?>
